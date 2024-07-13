36693dc2d83f58b58c344314a370aaf20e4efd2a
Gustavo Almeida <gustavomatos2277@gmail.com>
2024-07-13 16:38:26 -0300

    Adição das funcionalidades de criação de usuário e conta corrente + Finalização do desafio

--- desafio2.py
+++ desafio2.py
@@ -2,7 +2,9 @@ menu = '''
     1 - Depositar
     2 - Sacar
     3 - Extrato
-    4 - Sair
+    4 - Criar Usuário
+    5 - Criar Conta Corrente
+    6 - Sair
     
     Escolha uma opção:
 '''
@@ -43,7 +45,7 @@ def depositar(saldo, valor, extrato, /):
         
     return saldo, extrato
 
-def sacar(*, limite_saques, valor_limite, valor, saldo, extrato, ):
+def sacar(*, limite_saques, valor, saldo, extrato, ):
     if limite_saques == 0:
         print("Limite de saques diário atingido, tente novamente amanhã.")
     else:
@@ -65,6 +67,44 @@ def listar_extrato(saldo, /, *, extrato):
         print("Saldo: R$ {:.2f}".format(saldo))
         print("-" * 100)
 
+def filtrar_usuario(cpf, usuarios):
+    usuarios_filtrados = [usuario for usuario in usuarios if usuario[cpf] == cpf]
+    return usuarios_filtrados[0] if usuarios_filtrados else None
+
+def criar_usuario(usuarios):
+    cpf = input("Informe o CPF (somente números): ")
+    usuario = filtrar_usuario(cpf, usuarios)
+
+    if usuario:
+        print("O CPF informado já está sendo utilizado.")
+        return
+
+    nome = input("Informe o nome completo: ")
+    data_nasc = input("Informe a data de nascimento (no formato: dd/mm/aaaa):")
+    endereco = input("Informe o endereço (no formato: Logradouro, Num - Bairro - Cidade/Sigla do estado):")
+
+    usuarios.append({"nome:": nome, "data_nascimento": data_nasc, "cpf": cpf, "endereco": endereco})
+    print("Cadastro realizado com sucesso!")
+
+def criar_conta_corrente(agencia, numero_conta, usuarios):
+    cpf = input("Informe o CPF do usuário: ")
+    usuario = filtrar_usuario(cpf, usuarios)
+
+    if usuario:
+        print("Conta corrente criada com sucesso!")
+        return {"agencia": agencia, "numero_conta": numero_conta, "usuario": usuario} 
+    print("Usuário não encontrado, tente criar a conta com um usuário válido.")
+    return None
+
+def continuar_sessao(sessao, continuar):
+    if sessao == "S":
+        continuar = True
+        print("-" * 100)
+    else:
+        continuar = False
+        print("Sessão finalizada.")
+    return continuar
+
 while continuar:
     opcao = input(menu)
     print("-" * 100)
@@ -76,12 +116,7 @@ while continuar:
         saldo, extrato = depositar(saldo, valorDeposito, extrato)
 
         sessao = input(novaOperacao).upper()
-        if sessao == "S":
-            continuar = True
-            print("-" * 100)
-        else:
-            continuar = False
-            print("Sessão finalizada.")
+        continuar = continuar_sessao(sessao, continuar)
     elif opcao == "2":
         print(selecao.format("2 - Sacar"))
         print("") 
@@ -90,26 +125,28 @@ while continuar:
         saldo, extrato, saques_disponiveis = sacar(limite_saques = saques_disponiveis, valor = valorSaque, saldo = saldo, extrato = extrato)
 
         sessao = input(novaOperacao).upper()
-        if sessao == "S":
-            continuar = True
-            print("-" * 100)
-        else:
-            continuar = False
-            print("Sessão finalizada.")
+        continuar = continuar_sessao(sessao, continuar)
     elif opcao == "3":
-         print(selecao.format("3 - Extrato"))
-         print("")
+        print(selecao.format("3 - Extrato"))
+        print("")
 
-         sessao = input(novaOperacao).upper()
-         if sessao == "S":
-             continuar = True
-             print("-" * 100)
-         else:
-             continuar = False
-             print("Sessão finalizada.")
+        listar_extrato(saldo, extrato = extrato)
+
+        sessao = input(novaOperacao).upper()
+        continuar = continuar_sessao(sessao, continuar)
     elif opcao == "4":
-         print(selecao.format("4 - Sair"))
-         print("Sessão finalizada.")
-         break
+        print(selecao.format("4 - Criar Usuário"))
+        criar_usuario(usuarios)
+    elif opcao == "5":
+        print(selecao.format("5 - Criar Conta Corrente"))
+        numero_conta = len(contas_correntes) + 1
+        conta = criar_conta_corrente(agencia, numero_conta, usuarios)
+
+        if conta:
+            contas_correntes.append(conta)
+    elif opcao == "6":
+        print(selecao.format("6 - Sair"))
+        print("Sessão finalizada.")
+        break
     else:
-         print("Opção inválida, informe novamente.")
+        print("Opção inválida, informe novamente.")
